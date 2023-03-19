a07f82aea7c792d03f5e9a999d7aeda24fb30ad3
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-03-19 17:23:24 +0300

    Added documentation to the library

--- src/main/java/frc/robot/Mpu6050.java
+++ src/main/java/frc/robot/Mpu6050.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.I2C;
 
 public class Mpu6050 {
     private static final int power_mgmt_1 = 0x6b;
-    private static final int power_mgmt_2 = 0x6c;
     private static final int mpu6050_Adress = 0x68;
 
     private static final int gyro_x_out = 0x43;
@@ -17,10 +16,18 @@ public class Mpu6050 {
 
     public static final I2C mpu6050 = new I2C(I2C.Port.kOnboard, mpu6050_Adress);
 
+
+    /**
+     * Disables sleep mode on the mpu6050
+     */
     public void init() {
         mpu6050.write(power_mgmt_1, 0);
     }
-
+    /**
+     * Reads a 2 byte value from the mpu6050
+     * @param adress The adress to read from
+     * @return The value read
+     */
     public Integer read(int adress) {
         byte[] data = new byte[2];
         mpu6050.read(adress, 2, data);
@@ -28,10 +35,15 @@ public class Mpu6050 {
         return x;
     }
     
+    /**
+     * Calculates the distance between two points
+     * @param x The x value
+     * @param y The y value
+     * @return The distance between the two points
+     */
     public Double dist(Double x, Double y) {
         return Math.sqrt(x*x + y*y);
     }
-
 
     public Integer get_Gyro_x() {
         int x = read(gyro_x_out);
@@ -48,6 +60,10 @@ public class Mpu6050 {
         return z/131;
     }
 
+    /**
+     * Calculates the rotation of the robot on the x axis in terms of degrees
+     * @return The rotation of the robot on the x axis
+     */
     public Double get_x_Rotation() {
         double x = get_Gyro_x();
         double y = get_Gyro_y();
@@ -55,7 +71,10 @@ public class Mpu6050 {
         double radians = Math.atan2(x, dist(y,z));
         return -Math.toDegrees(radians);
     }
-    
+    /**
+     * Calculates the rotation of the robot on the y axis in terms of degrees
+     * @return The rotation of the robot on the y axis
+     */
     public Double get_y_Rotation() {
         double x = get_Gyro_x();
         double y = get_Gyro_y();
@@ -64,7 +83,7 @@ public class Mpu6050 {
         return Math.toDegrees(radians);
     }
 
-    
+
     public double get_Accel_x() {
         int x = read(accel_x_out);
         return x/16384.0;
@@ -74,7 +93,7 @@ public class Mpu6050 {
         int y = read(accel_y_out);
         return y/16384.0;
     }
-   
+    
     public double get_Accel_z() {
         int z = read(accel_z_out);
         return z/16384.0;
